dc8eadaa40a8ae5b4fb6a5a80c6c571763055a5b
desaahmeti <73140341+desaahmeti@users.noreply.github.com>
2021-05-31 23:01:20 +0200

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Rrezon Hasani',
+        username: 'Rrez',
+        email: 'rrezonhasani@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Drin Ahmeti',
+        username: 'Drin',
+        email: 'drinahmeti14@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Vala Mucolli',
+        username: 'Vale',
+        email: 'valamucolli@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Rina Kaciu',
+        username: 'Rina',
+        email: 'rinakaciu@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Risina Salihu',
+        username: 'Risa',
+        email: 'risinasalihu@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Elsa Fona',
+        username: 'Elsa',
+        email: 'elsafona@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Valja Mehmeti',
+        username: 'Valja',
+        email: 'valjamehmeti@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Lirika Maliqi',
+        username: 'Lirika',
+        email: 'lirikamaliqi@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Diellza Shabani',
+        username: 'Dixi',
+        email: 'diellzashabani@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Neta Kryeziu',
+        username: 'Neta',
+        email: 'netakryeziu@gmail.com'
+    },
+    {
+        id: 11,
+        name: 'Arbnor Puka',
+        username: 'PUKA',
+        email: 'arbnordfz@gmail.com'
+    },
+    {
+        id: 12,
+        name: 'Isa Selimi',
+        username: 'Isa',
+        email: 'isaselimi@gmail.com'
+    },
+    {
+        id: 13,
+        name: 'Blerina Rexhepi',
+        username: 'Blerina',
+        email: 'blerinarexhepi@gmail.com'
+    }
+]
